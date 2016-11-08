154eb4e30180b3f8c600c98e0f5cbfc338046720
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2016-11-08 14:25:10 +0800

    generate resourse XML

--- src/ios/genXML/genXML.js
+++ src/ios/genXML/genXML.js
@@ -1,24 +1,30 @@
 var fs = require('fs-extra');
-var dirName = 'Resourse/';
+var dirName = '../Resourse/';
 const rootPath = 'src/ios/';
-
+const xml = 'resourse.xml';
+//清空
+fs.writeFileSync(xml, '')
 
 function getAllFile(dirName) {
     var n = fs.readdirSync(dirName);
     n.forEach((item, index) => {
         var files = fs.statSync(dirName + item);
         if (files.isDirectory()) {
-            // console.log(dirName + item + '/');
-            getAllFile(dirName + item + '/');
+            // .svn .git delete
+            if (/^\.svn|\.git$/.test(item)) {
+                fs.removeSync(dirName + item)
+            }else{
+                getAllFile(dirName + item + '/');
+            }
         } else {
-            if (/\.h$/.test(item)) {
-                // console.log(item);
-                fs.appendFileSync('fix.xml',`<header-file src="src/ios/${dirName}${item}" />\n`)
+            if (/^\.DS_Store$/.test(item)) {
+                fs.removeSync(dirName + item);
+            }else if (/\.h$/.test(item)) {
+                fs.appendFileSync(xml,`<header-file src="src/ios/${dirName.slice(3)}${item}" />\n`)
             }else if(/\.m$/.test(item)){
-                fs.appendFileSync('fix.xml',`<source-file src="src/ios/${dirName}${item}" />\n\n`)
+                fs.appendFileSync(xml,`<source-file src="src/ios/${dirName.slice(3)}${item}" />\n\n`)
             }else if(/\.xib|png|wav|pch$/.test(item)){
-                // console.log(dirName + item);
-                fs.appendFileSync('fix.xml',`<resource-file src="src/ios/${dirName}${item}" />\n\n`)
+                fs.appendFileSync(xml,`<resource-file src="src/ios/${dirName.slice(3)}${item}" />\n\n`)
             }else{
                 console.log(dirName + item);
             }
@@ -26,36 +32,3 @@ function getAllFile(dirName) {
     })
 }
 getAllFile(dirName);
-
-
-/*getAllFiles(dirName,function (data) {
-    console.log(data)
-})
-
-
-function getAllFiles(dir, callback) {
-    var filesArr = [];
-    dir = ///$/.test(dir) ? dir : dir + '/';
-        (function dir(dirpath, fn) {
-            var files = fs.readdirSync(dirpath);
-            async(files, function (item, next) {
-                var info = fs.statSync(dirpath + item);
-                if (info.isDirectory()) {
-                    dir(dirpath + item + '/', function () {
-                        next();
-                    });
-                } else {
-                    filesArr.push(dirpath + item);
-                    callback && callback(dirpath + item);
-                    next();
-                }
-            }, function (err) {
-                !err && fn && fn();
-            });
-        })(dir);
-    return filesArr;
-}*/
-
-
-
-
